51b6aeb6b0b8052b9abff0f440cbaa48e1030d86
Herman <hermanconnor11@gmail.com>
2021-03-22 14:18:36 -0400

    Add starter files

---
+++ README.md
@@ -0,0 +1,5 @@
+ROCK PAPER SCISSORS
+
+This is the classic rock paper scissors game.
+Right now, it will be played in the console at first.
+In the future, I will revisit and add a front end to it.
---
+++ app.js
@@ -0,0 +1,27 @@
+console.log(
+  `
+Rock
+Paper
+Scissors
+`
+);
+
+/**
+ ***** BASIC GIT STUFF****
+ * 
+ - CREATE A NEW REPOSITORY ON THE COMMAND LINE
+
+echo "# rock-paper-scissors" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:hermanconnor/rock-paper-scissors.git
+git push -u origin main
+
+ - PUSH AN EXISTING REPOSITORY FROM THE COMMAND LINE
+
+git remote add origin git@github.com:hermanconnor/rock-paper-scissors.git
+git branch -M main
+git push -u origin main
+ */
