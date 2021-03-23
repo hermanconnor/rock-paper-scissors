0883a52f7987e2b5fe7d1bc714c581c86a029304
Herman <hermanconnor11@gmail.com>
2021-03-22 22:18:48 -0400

    Add basic functionality

--- app.js
+++ app.js
@@ -1,27 +1,59 @@
-console.log(
-  `
-Rock
-Paper
-Scissors
-`
-);
+'use strict';
+let playerScore = 0;
+let computerScore = 0;
 
-/**
- ***** BASIC GIT STUFF****
- * 
- - CREATE A NEW REPOSITORY ON THE COMMAND LINE
+function game(numRounds) {
+  for (let i = 0; i < numRounds; i++) {
+    const playerPick = prompt('Enter your choice: Rock, Paper, or Scissors');
+    console.log(playRound(playerPick, computerPlay()));
+  }
 
-echo "# rock-paper-scissors" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:hermanconnor/rock-paper-scissors.git
-git push -u origin main
+  if (playerScore === computerScore) {
+    console.log(
+      `IT'S A DRAW! Player Score: ${playerScore} Computer Score: ${computerScore}`
+    );
+  } else if (playerScore > computerScore) {
+    console.log(
+      `YOU WIN! Score: ${playerScore} Computer Score: ${computerScore}`
+    );
+  } else {
+    console.log(
+      `YOU LOSE! Computer Score: ${computerScore} Score: ${playerScore}`
+    );
+  }
+}
+game(5);
 
- - PUSH AN EXISTING REPOSITORY FROM THE COMMAND LINE
+// PLAY GAME FUNCTIONALITY
+function playRound(playerSelection, computerSelection) {
+  const playerInput = playerSelection.toLowerCase();
+  const computerInput = computerSelection.toLowerCase();
+  const winMessage = `You win! ${playerInput} beats ${computerInput}!`;
+  const loseMessage = `You lose! ${computerInput} beats ${playerInput}!`;
 
-git remote add origin git@github.com:hermanconnor/rock-paper-scissors.git
-git branch -M main
-git push -u origin main
- */
+  if (playerInput === computerInput) {
+    return `It's a draw! You chose ${playerInput} and the computer chose ${computerInput}`;
+  } else if (playerInput === 'rock' && computerInput === 'scissors') {
+    playerScore += 1;
+    return winMessage;
+  } else if (playerInput === 'paper' && computerInput === 'rock') {
+    playerScore += 1;
+    return winMessage;
+  } else if (playerInput === 'scissors' && computerInput === 'paper') {
+    playerScore += 1;
+    return winMessage;
+  } else {
+    computerScore += 1;
+    return loseMessage;
+  }
+}
+
+// COMPUTER PLAY
+function computerPlay() {
+  const options = ['Rock', 'Paper', 'Scissors'];
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  const computerPick = options[randomNumber];
+
+  return computerPick;
+}
